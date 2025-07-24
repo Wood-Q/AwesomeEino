0deb97735929f336096c6399a4a534940ac1c1dd
qiaohengkang <qiaohengkang@bytedance.com>
2025-07-24 15:52:39 +0800

    实现图嵌套示例编写

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"AwesomeEino/stage9"
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/joho/godotenv"
@@ -16,7 +17,17 @@ func main() {
 	ctx := context.Background()
 	// stage9.OrcGraphWithModel(ctx, map[string]string{"role": "cute", "content": "你好啊"})
 	// stage9.OrcGraphWithState(ctx, map[string]string{"role": "cute", "content": "你好啊"})
-	stage9.OrcGraphWithCallback(ctx, map[string]string{"role": "cute", "content": "你好啊"})
+	// stage9.OrcGraphWithCallback(ctx, map[string]string{"role": "cute", "content": "你好啊"})
+	g := stage9.OutSideOrcGraph(ctx)
+	r, err := g.Compile(ctx)
+	if err != nil {
+		panic(err)
+	}
+	result, err := r.Invoke(ctx, map[string]string{"role": "cute", "content": "你好啊"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
 
 	// r, err := stage10.Buildtest(ctx)
 	// if err != nil {
